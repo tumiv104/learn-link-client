f8f180b2b4878a56682fb1a3f2dee66945d32492
Tu <146899655+tumiv104@users.noreply.github.com>
2025-10-04 10:55:36 +0700

    fix conflict

--- src/components/dashboard/PaginationBar.tsx
+++ src/components/dashboard/PaginationBar.tsx
@@ -73,10 +73,10 @@ export function PaginationBar({
               <span>{t("previous")}</span>
               </PaginationPrevious> */}
               <button
-                onClick={() => setPage(Math.min(totalPages, page + 1))}
-                disabled={page === totalPages}
+                onClick={() => setPage(Math.max(1, page - 1))}
+                disabled={page === 1}
                 className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 h-9 py-2 has-[>svg]:px-3 gap-1 px-2.5 sm:pr-2.5${
-                page === totalPages
+                page === 1
                     ? "opacity-50 cursor-not-allowed"
                     : "hover:bg-gray-100"
                 }`}
