d3cb0c61bd2b456ad48722e4999c11e705aca401
Tu <146899655+tumiv104@users.noreply.github.com>
2025-10-14 21:28:12 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,17 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+
+      "react/jsx-key": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
